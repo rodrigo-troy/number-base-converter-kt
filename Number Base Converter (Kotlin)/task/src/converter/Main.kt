7da8597ade55d3ce8ea92aa2202d141d1aacd986
rodrigotroy <1328268+rodrigo-troy@users.noreply.github.com>
2023-03-11 22:41:04 -0300

    Stage 2

--- Number Base Converter (Kotlin)/task/src/converter/Main.kt
+++ Number Base Converter (Kotlin)/task/src/converter/Main.kt
@@ -1,7 +1,27 @@
 package converter // Do not delete this line
 
 fun main() {
-    println("Enter number in decimal system:")
+    while (true) {
+        println("Do you want to convert /from decimal or /to decimal?  (To quit type /exit)")
+        when (readln()) {
+            "/from" -> fromDecimal()
+            "/to" -> toDecimal()
+            "/exit" -> return
+            else -> println("Unknown option")
+        }
+    }
+}
+
+fun toDecimal() {
+    println("Enter source number:")
+    val number = readln()
+    println("Enter source base:")
+    val base = readln().toInt()
+    println("Conversion to decimal result: ${number.toInt(base)}")
+}
+
+fun fromDecimal() {
+    println("Enter a number in decimal system:")
     val number = readln().toInt()
     println("Enter target base:")
     val base = readln().toInt()
